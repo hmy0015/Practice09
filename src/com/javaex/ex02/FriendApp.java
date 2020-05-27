47a6ce8d36ab2ef28d949cf3c7b0f58b6b5577f2
hi <hi@DESKTOP-A3OJ2BV>
2020-05-27 19:55:40 +0900

    수정

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -5,17 +5,15 @@ public class FriendApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);     
 		List<Friend> fList = new ArrayList<Friend>();
-		int cnt = 0;
 
         System.out.println("친구를 3명 등록해 주세요");
-    	
-        while(cnt < 3) {
+
+        for(int i = 0; i < 3; i++) {
         	String fInfo = sc.nextLine();
         	String[] sArray = fInfo.split(" ");
         	
         	Friend friend = new Friend(sArray[0], sArray[1], sArray[2]);
         	fList.add(friend);
-        	cnt++;
         }
         
         for(int i = 0; i < fList.size(); i++) {
